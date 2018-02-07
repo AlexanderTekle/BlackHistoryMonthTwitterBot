a5e97d62508b193b82306dca55ebf2418ea77622
Alexander Tekle <tekle.alexander@gmail.com>
2018-02-06 21:00:33 -0600

    Setting up Twitter api configurations

---
+++ config.js
@@ -0,0 +1,13 @@
+//config.js
+/** TWITTER APP CONFIGURATION
+ * consumer_key
+ * consumer_secret
+ * access_token
+ * access_token_secret
+ */
+module.exports = {
+  consumer_key: 'PCTbWNscO0BMwTdo5rDRwvvM0',
+  consumer_secret: '9FgQV1uYcdwtr8VxwLaKHaKcMtcYOXX5Ndyp7W5v4gH3mriaaN',
+  access_token: '961046764714364928-Tjhfz6a0zJDWLcPtMIg1cASujKp0xfD',
+  access_token_secret: 'W5sQflbHny1SJEGKmtnVZvmgOrOtrslHtNEZELPp6MCif'
+}
